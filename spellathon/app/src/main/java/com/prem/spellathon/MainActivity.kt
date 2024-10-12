46d2f67f73fcd467b952f571353b8c2a9e69db44
premkumar bhaskal <pbhaskal@gmail.com>
2024-10-12 21:35:00 +0530

    added rules description

--- spellathon/app/src/main/java/com/prem/spellathon/MainActivity.kt
+++ spellathon/app/src/main/java/com/prem/spellathon/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
@@ -19,9 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prem.spellathon.ui.theme.SpellathonTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,25 +65,52 @@ fun SpellathonLayout( modifier: Modifier = Modifier) {
         Row (
             modifier = modifier,
         ) {
-            Column (
-                modifier = modifier.fillMaxWidth().weight(1f).requiredSize(150.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Image(painter = painterResource(R.drawable.spell_1),
-                    contentDescription = "Spell puzzle",
-                    modifier = modifier.requiredSize(150.dp))
-            }
-
-            Column(
-                modifier = modifier.fillMaxWidth().weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text (
-                    text = "Rules",
-                    modifier = modifier,
-                )
-            }
+            ImageSection(Modifier.weight(1f))
+            RuleSection(Modifier.weight(1f))
         }
+    }
+}
+
+@Composable
+private fun RuleSection(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Rules",
+            modifier = Modifier,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+        )
+        Text(
+            text = "How many words of four or more letters can you make from the letters shown in today's puzzle?" +
+                    "In making a word, each letter may be used once only. Each word must contain the central letter." +
+                    "There should be at least one seven-letter word. Plurals, foreign words and proper names are not allowed.",
+            modifier = Modifier.padding(start = 5.dp, end = 5.dp),
+            textAlign = TextAlign.Justify,
+            fontSize = 12.sp,
+        )
+    }
+}
+
+@Composable
+private fun ImageSection(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .requiredSize(175.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(
+            painter = painterResource(R.drawable.spell_1),
+            contentDescription = "Spell puzzle",
+            modifier = modifier
+//                .fillMaxSize()
+//                .fillMaxWidth()
+                        .requiredSize(160.dp)
+                        .padding(top = 10.dp)
+        )
     }
 }
 
